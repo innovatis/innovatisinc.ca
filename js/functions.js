9362b4e4c12cc51bda569a4462a5851737be4ad4
Stefan Penner <stefan.penner@gmail.com>
2011-01-30 22:16:01 -0600

    validation

--- js/functions.js
+++ js/functions.js
@@ -3,63 +3,71 @@ function throb(max,fcn){
 
   (function throbber(){
     $('#green_glow').fadeIn('slow',function(){
-      $(this).fadeOut('slow',function(){
-        count++;
-        if(count < max){
-          throbber();
-        }else if(fcn){
-          fcn();
-        }
-        }) 
+        $(this).fadeOut('slow',function(){
+          count++;
+          if(count < max){
+            throbber();
+          }else if(fcn){
+            fcn();
+          }
+        });
       });
-  })()
-};
+  })();
+}
 
-  function animateBusiness(start,stop){
-    var offset   = -100,
-        business = $('.business'),
-        start    = start || buildings[buildingIndex].small,
-        stop     = stop  || buildings[buildingIndex].large;
+function animateBusiness(newStart,newStop){
+  var offset   = -100,
+      business = $('.business'),
+      start    = newStart || buildings[buildingIndex].small,
+      stop     = newStop  || buildings[buildingIndex].large;
 
-    buildingIndex = (buildingIndex + 1)%3;
+  buildingIndex = (buildingIndex + 1)%3;
 
-    business.attr('id',start);
+  business.attr('id',start);
 
-    business.css({left:offset}).fadeIn(function(){
-      (function(){
-        offset += 3;
-        var continueAnimation = arguments.callee;
+  business.css({left:offset}).fadeIn(function(){
+    (function(){
+      offset += 3;
+      var continueAnimation = arguments.callee;
 
-        business.css({left:offset+"px"});
+      business.css({left:offset+"px"});
 
-        if(offset < 410 || offset > 410 && offset < 1000){
-          setTimeout(arguments.callee,30);
-        }else if(offset < 1000){
-          beltOn = false
+      if(offset < 410 || offset > 410 && offset < 1000){
+        setTimeout(arguments.callee,30);
+      }else if(offset < 1000){
+        beltOn = false;
 
-          throb(3,function(){ 
+        if($.support.opacity){
+          throb(3,function(){
             business.attr('id',stop);
             // funky transition
             offset = 410;
             beltOn = true;
             continueAnimation();
           });
-
-        }else if( offset >= 1000){
-          setTimeout(animateBusiness,2000);
+        }else{
+          setTimeout(function(){
+            business.attr('id',stop);
+            offset = 410;
+            beltOn = true;
+            continueAnimation();
+          },3000);
         }
-      })()
-    });
-  };
+      }else if( offset >= 1000){
+        setTimeout(animateBusiness,2000);
+      }
+    })();
+  });
+}
 
 
-  window.beltOn = true,
+  window.beltOn = true;
   window.houseOn = true;
   window.buildingIndex = 0;
   window.buildings = [
     {
       small: "small_store",
-      large: "large_store",
+      large: "large_store"
     },
     {
       small: "small_factory",
@@ -75,7 +83,7 @@ function throb(max,fcn){
   function animateBelt(){
     var counter  = 0,
         belts    = $('#belt-left,#belt-right');
-    
+
     (function(){
       if(beltOn){
         counter += 3;
@@ -84,15 +92,42 @@ function throb(max,fcn){
 
       setTimeout(arguments.callee,30);
     })();
+  }
+
+var Innovatis = Innovatis || {};
+
+(function(){
+
+  Innovatis.form = form = {};
+
+  form.validate = function(){
+    var element = $(this),
+    required    = element.attr('required'),
+    val         = $.trim(element.val()),
+    inputDiv    = element.closest('.input'),
+    type        = element.attr("type");
+
+    if(required && val === ''){
+      inputDiv.addClass("error");
+    }else{
+      inputDiv.removeClass("error");
+    }
   };
+
+  form.validateAll = function(){
+    $("input,textarea").each(form.validate);
+    return $(".input.error").length < 1;
+  };
+
+})();
 
 $(function(){
   $('.fadeIn').fadeIn(2000,function(){
     $('#belt-left,#belt-right').fadeIn(2000,function(){
       $('.business').fadeIn();
     });
-  });  
-  
+  });
+
   $('#contact_us_now').click(function(){
     $('#contact').stop().slideDown(1000,function(){
       $(window).scrollTo($('#contact'),1000);
@@ -106,10 +141,18 @@ $(function(){
 
   $('#form-close').click(function(){
     $(window).scrollTo(0,1000);
-    $('#contact').stop().slideUp(1100)
+    $('#contact').stop().slideUp(1100);
     return false;
   });
 
   animateBelt();
   animateBusiness();
+
+  $('#contact-form').submit(function(e){
+    var element = $(this);
+    if(!Innovatis.form.validateAll()){
+      e.preventDefault();
+    }
+  });
+  $('#contact-form input').keyup(Innovatis.form.validate);
 });
